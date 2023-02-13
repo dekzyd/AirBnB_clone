c68e8000d6456ea69c58d95c597eecfb85dd78b1
dekzyd <adekoyadayo2010@gmail.com>
2023-02-12 16:27:55 -0800

    Update hbnb

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Contains the Amenity model"""
+
+from models.base_model import BaseModel
+
+
+
+
+
+class Amenity(BaseModel):
+
+    """Implements the Amenity model"""
+
+    name = ""
