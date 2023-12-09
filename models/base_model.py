06cd8ae58e767c066d814ed944bd48bd1d7f05d7
Oladayo <adekoyadayo2010@gmail.com>
2023-12-09 08:03:38 -0800

    create FileStorage class  and update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,9 @@ class BaseModel:
 
     def save(self):
         '''saves the object'''
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         '''shows all object attributes in dictionary format'''
