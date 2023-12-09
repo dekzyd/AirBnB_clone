6fcf6db370ebad29f433c5dee503afacd3778b1d
Oladayo <adekoyadayo2010@gmail.com>
2023-12-09 05:10:20 -0800

    update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 '''defines all common attributes/methods for other classes'''
 
+
 class BaseModel:
     '''creates a basemodel for other classes'''
     def __init__(self, *args, **kwargs):
@@ -16,14 +17,15 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                        timefmt = '%Y-%m-%dT%H:%M:%S.%f'
+                        setattr(self, key, datetime.strptime(value, timefmt))
                     else:
                         setattr(self, key, value)
 
-
     def __str__(self):
         '''representation of the object as a string'''
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        strfmt = type(self).__name__, self.id, self.__dict__
+        return "[{}] ({}) {}".format(strfmt)
 
     def save(self):
         '''saves the object'''
@@ -35,4 +37,4 @@ class BaseModel:
         dictionary['__class__'] = type(self).__name__
         dictionary['updated_at'] = dictionary['updated_at'].isoformat()
         dictionary['created_at'] = dictionary['created_at'].isoformat()
-        return dictionary
+        return dictionar
