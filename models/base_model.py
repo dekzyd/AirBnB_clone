a320aa45917f72e825b8126977c0e04fc70f9afe
Oladayo <adekoyadayo2010@gmail.com>
2023-12-09 12:19:55 -0800

    update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,13 @@ class BaseModel:
     '''creates a basemodel for other classes'''
     def __init__(self, *args, **kwargs):
         '''initializes a basemodel object'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        storage.new(self)
 
-        if kwargs is not None:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
