9994a41179500acd74fb532f6f7c258570aac089
Oladayo <adekoyadayo2010@gmail.com>
2023-12-10 02:08:02 -0800

    fix basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,13 @@
 
 import uuid
 from datetime import datetime
-'''defines all common attributes/methods for other classes'''
+"""defines all common attributes/methods for other classes"""
 
 
 class BaseModel:
-    '''creates a basemodel for other classes'''
+    """creates a basemodel for other classes"""
     def __init__(self, *args, **kwargs):
-        '''initializes a basemodel object'''
+        """initializes a basemodel object"""
 
         from models import storage
         if not kwargs:
@@ -18,28 +18,28 @@ class BaseModel:
             storage.new(self)
         else:
             for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        timefmt = '%Y-%m-%dT%H:%M:%S.%f'
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        timefmt = "%Y-%m-%dT%H:%M:%S.%f"
                         setattr(self, key, datetime.strptime(value, timefmt))
                     else:
                         setattr(self, key, value)
 
     def __str__(self):
-        '''representation of the object as a string'''
+        """representation of the object as a string"""
         return "[{}] ({}) {}".format(
                 type(self).__name__,
                 self.id, self.__dict__
                     )
 
     def save(self):
-        '''saves the object'''
+        """saves the object"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        '''shows all object attributes in dictionary format'''
+        """shows all object attributes in dictionary format"""
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = type(self).__name__
         dictionary['updated_at'] = dictionary['updated_at'].isoformat()
